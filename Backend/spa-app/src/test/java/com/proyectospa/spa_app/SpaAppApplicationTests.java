c0468cfd63a90cb6cc90e552eb9387b8486b6154
JuampiBolanio <juampi360786@gmail.com>
2025-04-07 10:11:52 -0300

    Se creó el proyecto frontend y backend. Listo para codificar

---
+++ Backend/spa-app/src/test/java/com/proyectospa/spa_app/SpaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectospa.spa_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
