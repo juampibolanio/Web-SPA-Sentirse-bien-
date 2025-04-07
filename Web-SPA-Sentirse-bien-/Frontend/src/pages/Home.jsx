4ed0fb838150fdad54913c404ad699ef3176df19
Manuel0803 <manulautaro2003@gmail.com>
2025-04-07 14:26:27 -0300

    Corrijo submódulo y agrego imágenes

---
+++ Web-SPA-Sentirse-bien-/Frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar"
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenido a Spa</h1>
+      <p>Tu bienestar, nuestra prioridad.</p>
+    </div>
+  )
+}
+
+export default Home
