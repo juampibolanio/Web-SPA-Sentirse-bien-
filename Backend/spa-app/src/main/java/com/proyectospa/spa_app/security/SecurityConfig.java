f56ba262f4b11b631e456dbcd01246cfe1ebc50e
JuampiBolanio <juampi360786@gmail.com>
2025-05-03 19:39:28 -0300

    Configuracion realizada para deploy del backend

--- Backend/spa-app/src/main/java/com/proyectospa/spa_app/security/SecurityConfig.java
+++ Backend/spa-app/src/main/java/com/proyectospa/spa_app/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // o el puerto donde corre Vite
+                        .allowedOrigins("https://sentirsebiensp.netlify.app") // o el puerto donde corre Vite
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
